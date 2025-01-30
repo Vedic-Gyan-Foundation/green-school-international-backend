734f9d779fbe1feed5285330a97d01989611a20a
panchanan16 <dekapanchanan16@gmail.com>
2025-01-30 14:09:50 +0530

    public folder added

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello from the API!'
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
