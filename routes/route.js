d0711c3a33725320b455a3b7dfde7006afb806eb
panchanan16 <dekapanchanan16@gmail.com>
2025-02-27 15:25:50 +0530

    imgpath api created

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const multer  = require('multer')
 const path = require('path')
+const {
+  glob,
+  globSync,
+  globStream,
+  globStreamSync,
+  Glob,
+} = require('glob')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,16 +22,16 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post('/upload', upload.array('photos', 12), function (req, res, next) {
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
-    // console.log(req.files)
     res.status(200).json({msg: `file uploaded successfully!`})
-  })
+})
 
 
-
-router.get('/add-form', (req, res)=> {
-  res.render('./galleryPage.ejs')
+router.get('/get-images', async (req, res)=> {
+   const imgs = await glob(['**/gallery/*.{png,jpeg,jpg,PNG,JPG}'])
+   const imgPath = imgs.map((img)=> {
+      return img.replace(/\\/g, '/').replace('public', '')
+   })
+   res.status(200).json({imgPath})
 })
 
 
