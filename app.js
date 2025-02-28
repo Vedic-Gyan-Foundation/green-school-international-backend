f820245cdb7c54453eda15a3e88054ed67d75879
panchanan16 <dekapanchanan16@gmail.com>
2025-02-28 12:05:44 +0530

    gallery and academic calennde updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs')
+const cors = require('cors')
 
 const routes = require('./routes/route')
 const pageRoutes = require('./routes/pages')
@@ -8,6 +9,7 @@ const pageRoutes = require('./routes/pages')
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
