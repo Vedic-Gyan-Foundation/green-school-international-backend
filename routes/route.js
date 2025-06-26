30b509bc20d1ddd9901b62a23f3df5f0524646b3
panchanan16 <dekapanchanan16@gmail.com>
2025-06-26 12:21:29 +0530

    file added for mandatory doc

--- routes/route.js
+++ routes/route.js
@@ -19,6 +19,8 @@ const storage = multer.diskStorage({
     }
   })
 
+  // File added
+
 const upload = multer({ storage });
 
 router.post('/upload', upload.array('photos', 12), function (req, res, next) {
